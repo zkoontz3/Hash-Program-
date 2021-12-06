327864e569c407bf6ae84f30e5725ebfbe92dabf
amcclellan2001 <amcclell@highpoint.edu>
2021-12-06 08:12:03 -0500

    Initial commit

---
+++ HashingProgram.cpp
@@ -0,0 +1,28 @@
+/*
+Zach Koontz, Colin McCarry, Sterphen Binkley, Alex McClellan
+Dr. Williams
+CSC-2710
+3 December 2021
+Hashing Sorting Program
+*/
+
+#include<iostream>
+#include<cstdlib>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    //Variables and Array
+    int array[] = {1, 3, 2, 7, 5, 4, 8, 7};
+    int n = sizeof(a);
+
+    //Hashing Algorithm
+    for (int i = 0; i < n; i++)
+    {
+        if arr[i] >
+    }
+
+    return 0;
+}
