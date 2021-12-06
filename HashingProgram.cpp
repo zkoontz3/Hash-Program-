c0886f8507b7cdb7df3fbfed34e8bd02f2ee650b
amcclellan2001 <amcclell@highpoint.edu>
2021-12-06 08:33:32 -0500

    Added merge and non-merge forks

--- HashingProgram.cpp
+++ HashingProgram.cpp
@@ -9,22 +9,32 @@ Hashing Sorting Program
 #include<iostream>
 #include<cstdlib>
 #include<string>
-#include<sys/time.h>
+#include<chrono>
+#include<ctime>
 
 using namespace std;
 
 void merge(int arr[], int start, int mid, int end);
 void mergeHash(int arr[], int start, int end, int &compares);
-void mergeNonHash(int arr[], int start, int end, int &compares)
+void mergeNonHash(int arr[], int start, int end, int &compares);
 
 int main()
 {
     //Variables and Array
-    int array1[] = {1, 3, 2, 7, 5, 4, 8, 7}, array2[] = {1, 3, 2, 7, 5, 4, 8, 7}, m = sizeof(a) / sizeof(a[0]), n = sizeof(a) / 2, o = sizeof(a);
+    int array1[] = {1, 3, 2, 7, 5, 4, 8, 7}, array2[] = {1, 3, 2, 7, 5, 4, 8, 7}, m = 0, n = 7;
     int compares1, compares2;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
 
-    mergeHash(array1, m, o, compares1);
-    mergeNonHash(array2, m, o, compares2)
+    //Hashed Sort
+    start = std::chrono::system_clock::now();
+    mergeHash(array1, m, n, compares1);
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Hashed Merge Sort Time: " << elapsed_seconds.count() << "s\n";
+
+    //Non-Hashed Sort
+    mergeNonHash(array2, m, n, compares2);
 
     return 0;
 }
@@ -74,8 +84,8 @@ void mergeHash(int arr[], int start, int end, int &compares)
 {
   if(start < end){
      int mid = start + (end - start)/2;
-     mergeSort(arr,start,mid,compares);
-     mergeSort(arr,mid + 1,end,compares);
+     mergeHash(arr,start,mid,compares);
+     mergeHash(arr,mid + 1,end,compares);
      merge(arr, start, mid, end);
   }
   compares++;
@@ -85,8 +95,8 @@ void mergeNonHash(int arr[], int start, int end, int &compares)
 {
   if(start < end){
      int mid = start + (end - start)/2;
-     mergeSort(arr,start,mid,compares);
-     mergeSort(arr,mid + 1,end,compares);
+     mergeNonHash(arr,start,mid,compares);
+     mergeNonHash(arr,mid + 1,end,compares);
      merge(arr, start, mid, end);
   }
   compares++;
