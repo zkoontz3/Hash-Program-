caf8003fe809869a8a8c48c7fb18f482fdca6265
zkoontz3 <69996962+zkoontz3@users.noreply.github.com>
2021-12-06 08:16:07 -0500

    Update HashingProgram.cpp

--- HashingProgram.cpp
+++ HashingProgram.cpp
@@ -12,6 +12,9 @@ Hashing Sorting Program
 
 using namespace std;
 
+void merge(int arr[], int start, int mid, int end);
+void mergeSort(int arr[], int start, int end, int &compares);
+
 int main()
 {
     //Variables and Array
@@ -25,4 +28,56 @@ int main()
     }
 
     return 0;
+}
+
+void merge(int arr[], int start, int mid, int end)
+{
+   int length1 = mid - start + 1;
+   int length2 = end - mid;  
+
+    int leftArr[length1], rightArr[length2];
+
+  for (int i = 0; i < length1; i++)
+    leftArr[i] = arr[start + i];
+  for (int j = 0; j < length2; j++)
+    rightArr[j] = arr[mid + 1 + j];
+
+  int a, b, c;
+  a = 0;
+  b = 0;
+  c = start;
+
+  while (a < length1 && b < length2) {
+    if (leftArr[a] <= rightArr[b]) {
+      arr[c] = leftArr[a];
+      a++;
+    } else {
+      arr[c] = rightArr[b];
+      b++;
+    }
+    c++;
+  }
+
+  while (a < length1) {
+    arr[c] = leftArr[a];
+    a++;
+    c++;
+  }
+
+  while (b < length2) {
+    arr[c] = rightArr[b];
+    b++;
+    c++;
+  }
+}
+
+void mergeSort(int arr[], int start, int end, int &compares)
+{
+  if(start < end){
+     int mid = start + (end - start)/2;
+     mergeSort(arr,start,mid,compares);
+     mergeSort(arr,mid + 1,end,compares);
+     merge(arr, start, mid, end);
+  }
+  compares++;
 }
